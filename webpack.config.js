98764a21c353005c950191d502779d7f7ccf9e3b
root <root@SOI-OpenVDMv2-VE.localdomain>
2017-08-08 20:42:26 +0000

    little fixes

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
 //    path: __dirname,
     path: path.resolve(__dirname, "dist"),
-    publicPath: '/',
+    publicPath: '/sealog/',
     filename: 'bundle.js'
   },
   module: {
