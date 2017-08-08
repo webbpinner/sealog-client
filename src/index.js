070d05c9460742a34197ecee67e0dc93043ad457
Webb Pinner <webbpinner@gmail.com>
2017-08-08 16:40:52 -0400

    Fixing little things

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ if (token) {
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeSwitcher themePath="/themes" defaultTheme="yeti" storeThemeKey="theme">
+    <ThemeSwitcher themePath="./themes" defaultTheme="yeti" storeThemeKey="theme">
       <ConnectedRouter history={history}>
           <div>
             <Header />
